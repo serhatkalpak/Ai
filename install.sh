aa6c5d80c830d5dd5c497670396012a063f36e59
serhatkalpak <101672737+serhatkalpak@users.noreply.github.com>
2025-03-03 19:07:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
+echo "📦 Gereksinimler yükleniyor..."
 pkg update -y
 pkg install python git -y
 pip install --upgrade pip
 pip install -r requirements.txt
-echo 'alias ai-tool="python ~/termux-ai-tool/main.py"' >> ~/.bashrc
-source ~/.bashrc
+echo 'alias ai-tool="python $HOME/termux-ai-tool/main.py"' >> $HOME/.bashrc
+source $HOME/.bashrc
 echo "✅ Kurulum tamamlandı! 'ai-tool' yazarak başlatın."
