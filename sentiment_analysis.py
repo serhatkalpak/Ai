655086892752baed14afae79cb777d2d9e0cf91c
serhatkalpak <101672737+serhatkalpak@users.noreply.github.com>
2025-03-03 18:55:56 +0300

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,6 +2,6 @@ from transformers import pipeline
 
 def run():
     model = pipeline("sentiment-analysis")
-    text = input("Metin girin: ")
+    text = input("Metin: ")
     result = model(text)[0]
     print(f"Duygu: {result['label']}, Skor: {result['score']:.2f}")
